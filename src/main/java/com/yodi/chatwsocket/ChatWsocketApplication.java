f83c8d87692d98fb54199275fb9ebffeb07bc570
Yodi Satriani <yodi.satriani@fwd.com>
2019-03-17 00:24:26 +0800

    first commit

---
+++ src/main/java/com/yodi/chatwsocket/ChatWsocketApplication.java
@@ -0,0 +1,13 @@
+package com.yodi.chatwsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWsocketApplication.class, args);
+	}
+
+}
